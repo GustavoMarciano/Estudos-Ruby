6068e8db2edc2b4c1fd21e4332106b4a27e92731
Gustavo Marciano <gustavodexpace@gmail.com>
2022-06-09 22:17:38 -0300

    Primeiros exercicios resolvidos
    Primeiros exercicios resolvidos  em ruby

--- banco.rb
+++
@@ -1,6 +0,0 @@
-event_loop = true
-
-while event_loop = true
-    puts "Olá bem vindo ao sistema de cadastro de professores"
-
-end
---
+++ exercicio00.rb
@@ -0,0 +1,51 @@
+# 1 Cria duas variaveis com algum texto e imprimir elas fazendo concatenação.
+# 2 Crie duas variaveis com algum texto e imprimir elas fazendo interpolação.
+# 3 Crie uma variavel com essa variavel faça uma tabula do valor da variavel.
+# 4 Cria as variaveis nome, cidade, estado, rua, número da casa e cep. Imprima essas variáveis 
+# como se fosse um endereço completo.
+
+# 1 e 2
+ola = "Olá"
+mundo = "Mundo"
+
+puts ola + " " + mundo + " concatenação"
+puts "#{ola} #{mundo} interpolação"
+
+#3
+number = 5
+i = 0
+while i < 11
+    display_number = number * i
+    puts "#{i} x #{number} = #{display_number}"
+    i+=1
+end
+
+#4
+nome = ""
+cidade = ""
+estado = ""
+rua = ""
+numero = ""
+cep = ""
+
+
+#4
+puts "Qual o seu nome?"
+nome = gets.chomp
+
+puts "Qual a sua cidade?"
+cidade = gets.chomp
+
+puts "Qual a sua estado?"
+estado = gets.chomp
+
+puts "Qual a sua rua?"
+rua = gets.chomp
+
+puts "Qual a sua numero?"
+numero = gets.chomp
+
+puts "Qual a sua cep?"
+cep = gets.chomp
+
+puts "Olá #{nome} voce mora em #{cidade} no estado #{estado} rua #{rua} , #{numero} e cep: #{cep}?"
